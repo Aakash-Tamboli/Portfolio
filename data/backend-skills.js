bd2d63832bb8c2ccf0d6e7927e40726a6e775d07
Aakash-Tamboli <aakash11july@gmail.com>
2025-03-11 10:30:07 +0530

    {

---
+++ data/backend-skills.js
@@ -0,0 +1,8 @@
+const backEndSkills = [
+    { name: "Node", level: "Experienced" },
+    { name: "Express", level: "Intermediate" },
+    { name: "MongoDB", level: "Intermediate" },
+    { name: "SQL", level: "Intermediate" },
+    { name: "Java", level: "Experienced" },
+    { name: "C/C++", level: "Experienced" }
+];
---
+++ data/fontend-skills.js
@@ -0,0 +1,10 @@
+const frontEndSkills = [
+    { name: "HTML", level: "Intermediate" },
+    { name: "CSS", level: "Basic Level" },
+    { name: "Javascript", level: "Experienced" },
+    { name: "JQuery", level: "Intermediate" },
+    { name: "Bootstrap", level: "Basic Level" },
+    { name: "React", level: "Intermediate" },
+    { name: "Typescript", level: "Basic Level" },
+    { name: "Tailwind CSS", level: "Basic Level" }
+];
