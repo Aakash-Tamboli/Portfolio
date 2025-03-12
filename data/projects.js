ad00eb9e3a832f2b9319dd92e482699030672a23
Aakash-Tamboli <aakash11july@gmail.com>
2025-03-12 13:55:03 +0530

    implementing multi line description

--- data/projects.js
+++ data/projects.js
@@ -2,36 +2,38 @@ const projects = [
     {
         id: 3,
         title: "Bro HTTP Project",
-        description: `In my view, engineering is about building solutions that address real-world problems. To drive innovation, we must first understand and utilize existing technologies effectively, which ultimately paves the way for future advancements.
+        description: `<div> In my view, engineering is about building solutions that address real-world problems. To drive innovation, we must first understand and utilize existing technologies effectively, which ultimately paves the way for future advancements.
         This project showcases network programming (socket programming) in C++, where I developed a custom Bro Server that functions similarly to Tomcat or XAMPP. The server provides web developers with a familiar and useful tool to streamline their work.
-        Through this project, I gained deep insights into network communication, client-server interactions, and the fundamentals of how web servers operate. Moreover, I learned to troubleshoot and optimize network-based applications, which strengthened my problem-solving skills in system-level programming.
+        Through this project, I gained deep insights into network communication, client-server interactions, and the fundamentals of how web servers operate. Moreover, I learned to troubleshoot and optimize network-based applications, which strengthened my problem-solving skills in system-level programming. </div>
         `,
         tags: ["C++", "Foundation", "System Programming", "Thinking Machines"]
     },
     {
         id: 2,
         title: "Cpp Project",
-        description: `v3 Minor C++ Project which I made during learning C++. <br />
-
-        1) Student Management System - It is a fun project to understand, In general how management system works. <br />
-
-        2) Inventory - It is a General Quantity Based Measurement console based software which teach me how to develop a software in layered programing approach. <br />
-
-        3) Replica Of STL - It is also a fun project to understand the importance of virtual polymorphism and how it is implemented in predefined library/classes. <br />
-
-        4) tmcommon - It is just string utility project. <br />
-
-        5) TMDate - It is a kind of replica of Date class which present in Java, hence Traditional C++ won't provide anything related to Date. <br />
-
-        6) SQlite3 - RDMBS for c++ projects. <br />
-
+        description: `
+        <div>
+        Minor C++ Project which I made during learning C++.
+        <ul>
+            <li>Student Management System - It is a fun project to understand, In general how management system works.
+            </li>
+            <li>Inventory - It is a General Quantity Based Measurement console based software which teach me how to
+                develop a software in layered programing approach.</li>
+            <li>Replica Of STL - It is also a fun project to understand the importance of virtual polymorphism and how
+                it is implemented in predefined library/classes.</li>
+            <li>tmcommon - It is just string utility project.</li>
+            <li>TMDate - It is a kind of replica of Date class which present in Java, hence Traditional C++ won't
+                provide anything related to Date.</li>
+            <li>SQlite3 - RDMBS for c++ projects.</li>
+        </ul>
+    </div>
         `,
         tags: ["C++", "Foundation", "System Programming", "Thinking Machines"]
     },
     {
         id: 1,
         title: "DS Algo Library",
-        description: "we have STL in C++, Collection classes in Java but when we talk about C so there is no such facilites to handle some huge data and In this project I Implemeted data stuctures and some sorting algorithms for data.",
+        description: "<div> we have STL in C++, Collection classes in Java but when we talk about C so there is no such facilites to handle some huge data and In this project I Implemeted data stuctures and some sorting algorithms for data. </div>",
         tags: ["C", "Foundation", "System Programming", "Data Structure and Algorithm made easy by nasimha Sir", "C Project"]
     }
 ];
